7e7fc171746dd4eff0a39711363878a7767e6d72
Shilpi Verma <shilpiverma@Shilpis-MacBook-Pro.local>
2018-02-22 00:33:37 -0600

    added helper functions for the countdown timer

--- src/App.js
+++ src/App.js
@@ -10,44 +10,133 @@ class App extends React.Component{
   constructor(props){
     super(props);
     this.state={
-      break_duration:5,
-      session:25,
-      isTimerRunning:false,
-      timeElapsed:0
-
+      break_duration:300,
+      session:1500,
+      countdown:1500,
+      intervalID:null,
+      isSession:true,
+      isPaused:true,
+      isBreak:false,
+      displayedMinutes:'',
+      displayedSeconds:''
     }
-    this.handleDecrement=this.handleDecrement.bind(this);
-    this.handleIncrement=this.handleIncrement.bind(this);
+    this.handleBreakDecrement=this.handleBreakDecrement.bind(this);
+    this.handleBreakIncrement=this.handleBreakIncrement.bind(this);
     this.handleSessionIncrement=this.handleSessionIncrement.bind(this);
     this.handleSessionDecrement=this.handleSessionDecrement.bind(this);
+    this.onPauseTimer = this.onPauseTimer.bind(this);
+    this.onStartTimer = this.onStartTimer.bind(this);
+    this.togglePauseState = this.togglePauseState.bind(this); 
   }
 
 
-   
-  handleIncrement(){
+  
+
+  handleSessionIncrement(){
+    if(this.state.session<3600){
+      this.setState({
+        session:this.state.session+60,
+        countdown:this.state.countdown+60
+      },()=>
+        this.displayTime()
+      );
+      //else display an alert saying you reached the max limit 
+    }
+  }
+
+  handleSessionDecrement(){
+    if(this.state.session>900){
+      this.setState({
+        session:this.state.session-60,
+        countdown:this.state.countdown+60
+      },()=>this.displayTime());
+    }
+    //else display an alert you reached the min limit for a work session
+  }
+
+  handleBreakDecrement(){
+    if(this.state.break>300){
+      this.setState({
+        break_duration:this.state.break_duration-60
+      })
+    }
+    //else alert minimum break time is 5 minutes
+  }
+
+  handleBreakIncrement(){
+    if(this.state.break_duration<900){
+      this.setState({
+        break_duration:this.state.break_duration+60
+      })
+    }
+    //else alert that;s the maximum break allowed
+  }
+
+  componentwillMount(){
+    this.displayTime();
+  }
+
+  displayTime(number=this.state.countdown){
+    const countdown = number;
+    const minutes = Math.floor(countdown/60);
+    const seconds = countdown%60;
+    const displayedMinutes= minutes<10?'0'+minutes.toString():minutes.toString();
+    const displayedSeconds = seconds<10?'0'+seconds.toString:seconds.toString();
+
     this.setState({
-      break_duration:this.state.break_duration+1,
+      countdown:countdown,
+      displayedMinutes,
+      displayedSeconds
+    });
+  }
+
+  togglePauseState(){
+    this.setState({
+      isPaused:!this.state.isPaused
     })
   }
-  handleDecrement(){
-    this.setState({
-      break_duration:this.state.break_duration-1,
-    })
-  }
- handleSessionIncrement(){
-   this.setState({
-     session:this.state.session+1
-   })
- }
- handleSessionDecrement(){
-  this.setState({
-    session:this.state.session-1
-  })
-}
 
-componentDidMount(){
-  this.interval=setInterval(this.elapseTime,1000);
-}
+  elapsedTime(){
+    const countdown = this.state.countdown-1;
+    this.displayTime(countdown);
+  }
+
+  //pomodoro session
+  onStartTimer(){
+    if(this.state.isPaused){
+      this.displayTime();
+      this.setState({
+        intervalID:setInterval(this.elapsedTime,1000),
+        isPaused:!this.state.paused
+      });
+    }
+  }
+
+  //session on Pause
+  onPauseTimer(){
+    clearInterval(this.state.intervalID);
+  }
+
+
+  componentDidUpdate(){
+    if((this.state.countdown<0) && (this.state.isSession)){
+      this.setState({
+        isSession:!isSession,
+        isBreak:!isBreak,
+        countdown:this.state.break_duration
+      },()=>this.displayTime());
+    }
+    else if(this.state.countdown<0 && this.state.isBreak){
+      this.onPauseTimer();
+      this.setState({
+        isSession:!this.state.isSession,
+        isBreak:!this.state.isBreak,
+        countdown:this.state.session,
+        isPaused:!this.state.isPaused
+      },()=>this.displayTime());
+
+    }
+    }
 
 
   render(){
@@ -65,8 +154,10 @@ componentDidMount(){
         
          />
         <Timer
-        
-        session={this.state.session}
+        onStartTimer={this.onStartTimer}
+        displayedMinutes={this.state.displayedMinutes}
+        displayedSeconds={this.state.displayedSeconds}
+        onPauseTimer={this.onPauseTimer}
          />
       </div>
     )
--- src/Timer.js
+++ src/Timer.js
@@ -3,14 +3,9 @@ import SessionLength from './SessionLength';
 
 class Timer extends React.Component{
     render(){
-        const session_length = this.props.session;
-        
-       
-        
         return(
             <div>
                 <p>Session</p>
-                {session_length}
                 <button >start</button>
                 <button>stop</button>
                 <button>reset</button>
